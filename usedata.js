a7c6f3e0a7cc84de297d7492bcda305f97d8e0ee
ccyyu <86118757+ccyyu@users.noreply.github.com>
2021-06-18 23:27:55 +0800

    Create usedata.js

---
+++ usedata.js
@@ -0,0 +1,114 @@
+// shanghaijing used as other animals
+var usedata = {
+    'type': 'FeatureCollection',
+    'features': [
+        {
+            'type': 'Feature',
+            'properties': {
+                'name': '玉藻前',
+                'id': 134,
+                'place':'',
+                'description1': '<strong>原九尾狐:</strong>《山海经》记载：其状为狐有九尾，其音如婴，能食人。此后，大禹娶涂山氏、商纣王的宠妃妲己，都是以山海经中的九尾狐而来。',
+                'description2': '<strong>日韩：</strong >而日韩东南亚的九尾狐传说，相对就晚得多。根据传说，九尾狐妲己被斩首之后，元神不灭，游走于东方世界。比如，在日本平安时代，就有一位鸟羽天皇，娶了一位从唐朝来的美貌女人，叫做玉藻前。但没过多久，鸟羽天皇开始病痛缠身。 '
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [138.70387,35.863123],
+                'coordinatesbefore': [112.787018,24.954709]
+            }
+        },
+
+        {
+            'type': 'Feature',
+            'properties': {
+                'name': '旱魃',
+                'id': 135,
+                'place':'',
+                'description1': '<strong>原旱魃，山海经中，旱魃是皇帝的女儿，面貌丑陋，却心地善良。所到之处就会有大旱灾。</strong>轩辕黄帝正是利用旱魃的这一能力，破除了蚩尤的暴雨，最终赢得了战争。',
+                'description2': '<strong>日本：</strong>在日本神话中，旱魃的能力于此如出一辙。只是形象上有所不同，这可能跟山海经流传的版本差异有关。'
+            },
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [140.218728, 38.145717],
+                'coordinatesbefore': [108.298439, 35.783847]
+        }
+        },
+        
+        {
+            'type': 'Feature',
+            'properties': {
+                'name': '人鱼',
+                'id':136 ,
+                'place':'',
+                'description1': '<strong>原氐人国之人：</strong>人鱼的形象在山海经中多次出现。',
+                'description2': '<strong>日本：</strong>日本的人鱼妖怪形象和山海经一致。只是在日本的传说中，人鱼是一种预言灾祸的妖怪，贴在家里可以免除灾祸。'
+            },
+                'geometry': {
+                'type': 'Point',
+                'coordinates': [140.669728,38.497717],
+                'coordinatesbefore': [105.47217,32.830045]
+        }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'name': '烛阴',
+                'id':137 ,
+                'place':'',
+                'description1': '<strong>原烛龙：</strong >在山海经中，烛阴又称烛龙。是一位神格极高的存在，素有睁眼为昼，闭眼为夜，呼吸云雨的能力。也可以称为日月气象的象征。其主要形象为人首蛇身。',
+                'description2': '<strong>日本：</strong>烛阴的形象并没有发生多少变化，只是头部变成了女性。与中国古时的吃人美人蛇很相似。'
+            },
+                'geometry': {
+                'type': 'Point',
+                'coordinates': [134.488237,35.246703],
+                'coordinatesbefore': [88.799109,47.971461]
+        }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'name': '八岐大蛇',
+                'id':138 ,
+                'place':'',
+                'description1': '<strong>原相柳</strong>',
+                'description2': '<strong>日本：</strong>是日本神话中的怪物，别称八俣远吕智、八俣远吕知，一般被认为是某种强大的妖怪或祸神。它是一种能带来灾难的凶猛巨蛇，有着八头八尾的可怕外貌，其身体内蕴藏一把传说中的宝剑。它在七年间吃了奇稻田姬的姐妹，但最后在第八年被英雄须佐之男斩杀。'
+            },
+                'geometry': {
+                'type': 'Point',
+                'coordinates': [130.468427,31.915559],
+                'coordinatesbefore': [98.232491,40.895603]
+        }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'name': '犬神',
+                'id':139 ,
+                'place':'',
+                'description1': '<strong>原犬封国，犬神是附着到人身上的狗的死魂，其实来自中国《山海经》中的犬怪。</strong>据《山海经·海内北经》记载：犬封国（一说犬戎国），那里的人都是狗的模样。犬封国有一女子，正跪在地上捧着一杯酒食向人进献。',
+                'description2': '<strong>日本：</strong>鸟山石燕《画图百鬼夜行》中的犬神穿衣戴帽姿态傲慢地坐着，旁边一女童表情谦恭地伏在地上挥笔抄写着什么。两个故事都是一人一狗，同样都是人侍奉狗。'
+            },
+                'geometry': {
+                'type': 'Point',
+                'coordinates': [133.467187,33.656041],
+                'coordinatesbefore': [105.770371,35.377233]
+        }
+        },
+        {
+            'type': 'Feature',
+            'properties': {
+                'name': '川赤子',
+                'id':140 ,
+                'place':'',
+                'description1': '<strong>原蛊雕:</strong>蛊雕是一种生长在河边的野兽，外形像雕鹰，头上长有角，叫声如婴儿啼哭，能吃人。',
+                'description2': '<strong>日本：</strong>川赤子又名“河婴儿”，常在池塘、沼泽附近出现的妖怪。它喜欢在夜晚时模仿婴儿的哭声引诱、加害路人。'
+            },
+                'geometry': {
+                'type': 'Point',
+                'coordinates': [140.366173,35.842528],
+                'coordinatesbefore': [120.603596,29.995052]
+        }
+        },
+
+    ]
+};
